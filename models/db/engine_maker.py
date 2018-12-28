0ede18246fdcf70561c2433da2839d21c553d1c4
Nik <n.skubak@storytelling-software.com>
2018-12-28 12:27:01 +0300

    Fixed engine_maker

--- models/db/engine_maker.py
+++ models/db/engine_maker.py
@@ -6,10 +6,10 @@ from models.environment_settings import EnvironmentSettings
 class EngineMaker:
     @staticmethod
     def make_engine():
-        db_login = EnvironmentSettings._get_var_from_env('DB_LOGIN')
-        db_password = EnvironmentSettings._get_var_from_env('DB_PASSWORD')
-        db_host = EnvironmentSettings._get_var_from_env('DB_HOST')
-        db_db = EnvironmentSettings._get_var_from_env('DB_DB')
+        db_login = EnvironmentSettings.get_var_from_env('DB_LOGIN')
+        db_password = EnvironmentSettings.get_var_from_env('DB_PASSWORD')
+        db_host = EnvironmentSettings.get_var_from_env('DB_HOST')
+        db_db = EnvironmentSettings.get_var_from_env('DB_DB')
 
         engine_string = 'postgresql+psycopg2://' + db_login + ':'
         engine_string += db_password + '@' + db_host + '/' + db_db
