27c06bc848606a2a512222e0eb2d02505d561319
Nik <n.skubak@storytelling-software.com>
2018-12-28 12:52:25 +0300

    Modified engine_maker_test

--- tests/unit/models/db/engine_maker_test.py
+++ tests/unit/models/db/engine_maker_test.py
@@ -4,6 +4,7 @@ import os
 from sqlalchemy.engine import Engine
 
 from models.db.engine_maker import EngineMaker
+from exceptions.environment.variable_cant_be_import import VariableCantBeImport
 
 
 class EngineMakerTest(unittest.TestCase):
@@ -20,3 +21,11 @@ class EngineMakerTest(unittest.TestCase):
         os.environ['DB_DB'] = 'test'
         engine = EngineMaker.make_engine()
         self.assertIsInstance(engine, Engine)
+        os.environ.pop('DB_LOGIN')
+        os.environ.pop('DB_PASSWORD')
+        os.environ.pop('DB_HOST')
+        os.environ.pop('DB_DB')
+
+    def test_failed_engine_init(self):
+        with self.assertRaises(VariableCantBeImport):
+            EngineMaker.make_engine()
