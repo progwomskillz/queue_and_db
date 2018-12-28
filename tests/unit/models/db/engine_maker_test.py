8cc51e6a2ae0ae6d5e12482aad8be5e98b561923
Nik <n.skubak@storytelling-software.com>
2018-12-28 12:20:10 +0300

    Added engine_maker_test

---
+++ tests/unit/models/db/engine_maker_test.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+
+from sqlalchemy.engine import Engine
+
+from models.db.engine_maker import EngimeMaker
+
+
+class EngineMakerTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_engine_init(self):
+        os.environ['DB_LOGIN'] = 'test'
+        os.environ['DB_PASSWORD'] = 'test'
+        os.environ['DB_HOST'] = 'test'
+        os.environ['DB_DB'] = 'test'
+        engine = EngineMaker.make_engine()
+        self.assertIsInstance(engine, Engine)
