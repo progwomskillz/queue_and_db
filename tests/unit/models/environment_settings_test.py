c9af29bb2357737988a37b9698527b2f2612ddba
Nik <n.skubak@storytelling-software.com>
2018-12-28 12:48:17 +0300

    Added environment_settings_test

---
+++ tests/unit/models/environment_settings_test.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+
+from models.environment_settings import EnvironmentSettings
+from exceptions.environment.variable_cant_be_import import VariableCantBeImport
+
+
+class EnvironmentSettingsTests(unittest.TestCase):
+    def setUp(self):
+        self.key = 'TEST_VARIABLE'
+        self.value = 'TEST_STRING'
+
+    def tearDown(self):
+        pass
+
+    def test_get_available_var(self):
+        os.environ[self.key] = self.value
+        value = EnvironmentSettings.get_var_from_env(self.key)
+        self.assertEqual(value, self.value)
+        os.environ.pop(self.key)
+
+    def test_get_unavailable_var(self):
+        with self.assertRaises(VariableCantBeImport):
+            EnvironmentSettings.get_var_from_env(self.key)
