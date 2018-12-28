7d69d7ea8f16908e7f5030601400d9d8292b7e17
Nik <n.skubak@storytelling-software.com>
2018-12-28 16:20:53 +0300

    Added invoker

---
+++ models/invoker.py
@@ -0,0 +1,32 @@
+from requests.exceptions import RequestException
+from sqlalchemy.exc import SQLAlchemyError
+
+from exceptions.commands.command_runtime_error import CommandRuntimeError
+
+
+class Invoker:
+    def __init__(self, commands):
+        self.commands = commands
+        self.command_history = []
+
+    def execute_commands(self):
+        for command in self.commands:
+            try:
+                self.__execute_command(command)
+            except RequestException:
+                self.__undo_commands()
+                message = 'Request error. Check your network connection'
+                raise CommandRuntimeError(message)
+            except SQLAlchemyError:
+                self.__undo_commands()
+                message = 'SQL error'
+                raise CommandRuntimeError(message)
+
+    def __execute_command(self, command):
+        if command.execute():
+            self.command_history.append(command)
+
+    def __undo_commands(self):
+        for i in range(len(self.command_history)):
+            command = self.command_history.pop()
+            command.undo()
