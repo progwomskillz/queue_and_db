1ce855ce8626aa7990491d08ca9d1d5d21f8829f
Nik <n.skubak@storytelling-software.com>
2018-12-29 14:45:29 +0300

    Changed commands tests

--- tests/unit/commands/send_email_command_test.py
+++ tests/unit/commands/send_email_command_test.py
@@ -1,40 +1,17 @@
 import unittest
 from unittest.mock import MagicMock
-import os
-
-from requests.exceptions import RequestException
 
 from commands import SendEmailCommand
-from exceptions.commands import CommandCantBeUndone
-from models.services import EmailService
 
 
 class SendEmailCommandTest(unittest.TestCase):
     def setUp(self):
-        self.os_dict_keys = ['EMAIL_URL', 'EMAIL_TOKEN']
-        for key in self.os_dict_keys:
-            os.environ[key] = 'test_var'
-        message = {
-            'from': 'Man <man@example.com>',
-            'to': 'admin@example.com',
-            'subject': 'Success',
-            'text': 'Write successfully!'
-        }
-        email_service = EmailService(message)
-        self.command = SendEmailCommand(email_service)
+        self.email_service = MagicMock()
+        self.command = SendEmailCommand(self.email_service)
 
     def tearDown(self):
-        for key in self.os_dict_keys:
-            os.environ.pop(key)
+        pass
 
     def test_execute(self):
-        self.command.email_service.send = MagicMock(return_value=True)
         self.assertFalse(self.command.execute())
-
-    def test_failed_execute(self):
-        with self.assertRaises(RequestException):
-            self.assertFalse(self.command.execute())
-
-    def test_failed_undo(self):
-        with self.assertRaises(CommandCantBeUndone):
-            self.command.undo()
+        self.email_service.send.assert_called_once()
--- tests/unit/commands/write_to_db_command_test.py
+++ tests/unit/commands/write_to_db_command_test.py
@@ -1,42 +1,21 @@
 import unittest
 from unittest.mock import MagicMock
-import os
-
-from sqlalchemy.exc import SQLAlchemyError
 
 from commands import WriteToDbCommand
-from models.db import SessionMaker, EngineMaker, User
-from models.services import DBService
 
 
 class WriteToDbCommandTest(unittest.TestCase):
     def setUp(self):
-        self.os_dict_keys = ['DB_LOGIN', 'DB_PASSWORD', 'DB_HOST', 'DB_DB']
-        for key in self.os_dict_keys:
-            os.environ[key] = 'test_var'
-
-        engine = EngineMaker.make_engine()
-        session = SessionMaker.make_session(engine)
-        user = User(id=1, first_name='Nik', last_name='S.')
-        db_service = DBService(session, user)
-        self.command = WriteToDbCommand(db_service)
+        self.db_service = MagicMock()
+        self.command = WriteToDbCommand(self.db_service)
 
     def tearDown(self):
-        for key in self.os_dict_keys:
-            os.environ.pop(key)
+        pass
 
     def test_execute(self):
-        self.command.db_service.save = MagicMock(return_value=True)
         self.assertTrue(self.command.execute())
-
-    def test_failed_execute(self):
-        with self.assertRaises(SQLAlchemyError):
-            self.assertTrue(self.command.execute())
+        self.db_service.save.assert_called_once()
 
     def test_undo(self):
-        self.command.db_service.delete = MagicMock(return_value=True)
         self.command.undo()
-
-    def test_failed_undo(self):
-        with self.assertRaises(SQLAlchemyError):
-            self.command.undo()
+        self.db_service.delete.assert_called_once()
