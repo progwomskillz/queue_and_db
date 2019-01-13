732fc13794c75a93cff070ffc88596393ea9bf49
Nik <n.skubak@storytelling-software.com>
2019-01-13 15:21:12 +0300

    Refactored InvokerTest

--- tests/unit/invoker_test.py
+++ tests/unit/invoker_test.py
@@ -1,52 +1,55 @@
 import unittest
 from unittest.mock import MagicMock
-import os
 
-from models import Invoker
-from models.db import EngineMaker, SessionMaker, User
-from models.services import DBService, EmailService
-from commands import WriteToDbCommand, SendEmailCommand
-from exceptions.commands import CommandRuntimeError
+from requests.exceptions import RequestException
+from sqlalchemy.exc import SQLAlchemyError
+
+from invoker import Invoker
+from infrastructure.exceptions import CommandRuntimeError
 
 
 class InvokerTest(unittest.TestCase):
     def setUp(self):
-        commands = []
+        db_command = MagicMock()
+        email_command = MagicMock()
 
-        self.os_dict_keys = ['DB_LOGIN', 'DB_PASSWORD', 'DB_HOST', 'DB_DB',
-                             'EMAIL_URL', 'EMAIL_TOKEN']
-        for key in self.os_dict_keys:
-            os.environ[key] = 'test_var'
-
-        engine = EngineMaker.make_engine()
-        session = SessionMaker.make_session(engine)
-        user_object = User(id=1, first_name='Nik', last_name='S.')
-        db_service = DBService(session, user_object)
-        write_to_db_command = WriteToDbCommand(db_service)
-        commands.append(write_to_db_command)
-
-        message = {
-            'from': 'Man <man@example.com>',
-            'to': 'admin@example.com',
-            'subject': 'Success',
-            'text': 'Write successfully!'
-        }
-        email_service = EmailService(message)
-        send_email_command = SendEmailCommand(email_service)
-        commands.append(send_email_command)
-
+        commands = [db_command, email_command]
         self.invoker = Invoker(commands)
 
     def tearDown(self):
-        for key in self.os_dict_keys:
-            os.environ.pop(key)
+        pass
 
     def test_execute_commands(self):
-        self.invoker.commands[0].db_service.save = MagicMock(return_value=True)
-        self.invoker.commands[1].email_service.send = MagicMock(
-            return_value=True)
         self.invoker.execute_commands()
+        for command in self.invoker.commands:
+            command.execute.assert_called_once()
 
-    def test_failed_execute_commands(self):
+    def test_execute_command_with_request_error(self):
+        self.invoker.commands[1].execute = MagicMock(
+            side_effect=RequestException()
+        )
+
         with self.assertRaises(CommandRuntimeError):
-            self.invoker.execute_commands()
+            with self.assertRaises(RequestException):
+                self.invoker.execute_commands()
+
+                for command in self.invoker.commands:
+                    command.execute.assert_called_once()
+
+                for command in self.invoker.command_history:
+                    command.undo.assert_called_once()
+
+    def test_execute_command_with_sql_error(self):
+        self.invoker.commands[0].execute = MagicMock(
+            side_effect=SQLAlchemyError()
+        )
+
+        with self.assertRaises(CommandRuntimeError):
+            with self.assertRaises(SQLAlchemyError):
+                self.invoker.execute_commands()
+
+                for command in self.invoker.commands:
+                    command.execute.assert_called_once()
+
+                for command in self.invoker.command_history:
+                    command.undo.assert_called_once()
